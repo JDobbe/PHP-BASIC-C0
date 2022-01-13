a42240d19dfbacc7823e20d4b6d0166e653f2dcf
JDobbe <jeroen.dobbe@student.rocva.nl>
2022-01-13 11:46:00 +0100

    commit

--- 3-Arrays/3.1 - Array variabele/index.php
+++ 3-Arrays/3.1 - Array variabele/index.php
@@ -1,13 +1,18 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 <body>
-
+  
 <?php
-$abcGetallen = [['a', 'b', 'c', 'd'], ['24', '56', '78', '90']];
-echo $abcGetallen[0][0];
-echo $abcGetallen[1][1];
-
+$getallen = ['676', '5345', '78657', '3', '46', '21', '47'];
+asort($getallen);
+print_r( $getallen );
 ?>
 
 </body>
-</html> 
+</html>
